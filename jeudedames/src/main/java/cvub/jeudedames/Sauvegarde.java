e891cef89d9346e53a376a8437c83ec2c57ba58f
utaBdN <utaabn@gmail.com>
2025-12-08 14:41:11 +0100

    Create Sauvegarde.java

---
+++ jeudedames/src/main/java/cvub/jeudedames/Sauvegarde.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cvub.jeudedames;
+
+/**
+ *
+ * @author utaab
+ */
+public class Sauvegarde {
+    private String path;
+
+    public Sauvegarde(String path) {
+        this.path = path;
+    }
+    
+    
+    
+    
+    
+}
