581f6588ae1025f6aea590bd4bffc42c28938333
utaBdN <utaabn@gmail.com>
2025-12-08 17:41:21 +0100

    Implementation test de sauvegarde

--- jeudedames/src/main/java/cvub/jeudedames/Jeudedames.java
+++ jeudedames/src/main/java/cvub/jeudedames/Jeudedames.java
@@ -15,17 +15,17 @@ public class Jeudedames {
     public static void main(String[] args) throws IOException {
         
         Jeu jeu = new Jeu();
-        // jeu.initJeu();
+        jeu.initJeu();
         
-        Sauvegarde s = new Sauvegarde();
-        jeu.getJ1().add(new Pion(new Case(3,3), true));
-        s.save(jeu, "1.dame");
-        jeu.getJ1().get(0).setDame(false);
-        System.out.println("estDame : " + jeu.getJ1().get(0).isDame());
-        jeu = s.load("1.dame");
-        System.out.println("J1 length : " + jeu.getJ1().size());
-        System.out.println("Ok");
-        System.out.println("estDame : " + jeu.getJ1().get(0).isDame());
+//        Sauvegarde s = new Sauvegarde();
+//        jeu.getJ1().add(new Pion(new Case(3,3), true));
+//        s.save(jeu, "1.dame");
+//        jeu.getJ1().get(0).setDame(false);
+//        System.out.println("estDame : " + jeu.getJ1().get(0).isDame());
+//        jeu = s.load("1.dame");
+//        System.out.println("J1 length : " + jeu.getJ1().size());
+//        System.out.println("Ok");
+//        System.out.println("estDame : " + jeu.getJ1().get(0).isDame());
 
         Ui ui = new Ui();
         ui.displayJeu(jeu);
