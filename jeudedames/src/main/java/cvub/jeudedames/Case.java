0007d99fec72973e4e6ec0d36e90d9fc2788b5fd
clement-vazeille <clement.vazeille@gmail.com>
2025-12-08 15:20:46 +0100

    ui files created
    added a case file which represent a case in the game (x and y position)
    added a ui file which is used to show the game to the player and ask him inputs

---
+++ jeudedames/src/main/java/cvub/jeudedames/Case.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cvub.jeudedames;
+
+/**
+ *
+ * @author Clément
+ */
+public class Case {
+    
+    private int x; //between 0 and 4    
+    private int y; //between 0 and 9
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    
+    public Case(int coordUI)
+    {
+        coordUI--;
+        this.x = coordUI % 5;
+        this.y = coordUI % 5;
+    }
+    
+    public Case(int ix, int iy)
+    {
+        this.x=ix;
+        this.y=iy;
+    }
+}
---
+++ jeudedames/src/main/java/cvub/jeudedames/ui.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cvub.jeudedames;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Clément
+ */
+public class ui {
+    
+    public void uiTourDeJeu(boolean tourDuJoueur1)
+    {
+        
+    }
+    
+    public void
+    
+    private Case inputCase()
+    {
+        Scanner sc = new Scanner(System.in);
+        int inputCaseID = 0;
+        
+        while (true)
+        {
+            System.out.println("Veuillez entrer une case valide (1-50)");
+            if (sc.hasNextInt()) {
+                inputCaseID = sc.nextInt();
+                if(inputCaseID <51 && inputCaseID >0)
+                {
+                    return new Case(inputCaseID);
+                }
+                System.out.println("Entree invalide ! Veuillez entrer un nombre entre 1 et 50");
+            } else {
+                System.out.println("Entree invalide ! Veuillez entrer un nombre");
+                sc.next();
+            }
+        }
+
+    }
+}
